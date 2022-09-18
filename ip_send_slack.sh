7428d012d5d0dad0ebc564dc0894ffa548cfe055
DeviceManager <rene.geers@gmail.com>
2022-09-18 17:29:16 +0200

    Some minor modifications to sending local and global addresses

--- ip_send_slack.sh
+++ ip_send_slack.sh
@@ -3,40 +3,37 @@
 dir=$(cd $(dirname $0); pwd)
 
 slack_post () {
-curl -m 10 -X POST --data-urlencode \
+curl -m 10 -s -X POST --data-urlencode \
   "payload={ \"channel\": \"$4\",
              \"username\": \"$2\",
              \"text\": \"$3\",
              \"icon_emoji\": \":raspberry:\"}" $1
 }
 
-#Replace
-#slack_URL='https://hooks.slack.com/services/XXXXXXXXXXXXXXXXXXXXXXXX'
 slack_URL=${SLACK_TOKEN}
 #Give the Raspberry Pi a Recognizable Name
 name=${RPI_NAME}
 
 flag=true
 wlan_ip=$(ip a show wlan0 | grep "inet " | cut -f6 -d ' ')
-if "${flag}" ; then
+if test -n $wlan_ip; then
   text="wlan0="$wlan_ip
   echo $text
-  slack_post $slack_URL $name $text ${SLACK_CHANNEL} && flag=false
+  slack_post $slack_URL $name $text ${SLACK_CHANNEL} 
 fi
 
 eth_ip=$(ip a show eth0 | grep "inet " | cut -f6 -d ' ')
-if "${flag}" ; then
+if  test -n $eth_ip; then
   text="eth0="$eth_ip
   echo $text
-  slack_post $slack_URL $name $text ${SLACK_CHANNEL} && flag=false
+  slack_post $slack_URL $name $text ${SLACK_CHANNEL}
 fi
 
 g_ip=$(curl -m 10 inet-ip.info)
-if "${flag}" ; then
+if test -n $g_ip; then
   text="global_IP_address="$g_ip
   echo $text
   slack_post $slack_URL $name $text ${SLACK_CHANNEL} && flag=false
-
 fi
 
 if "${flag}" ; then
